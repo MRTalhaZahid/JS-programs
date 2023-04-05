609ff15484dd4fe9e978211a3108fbb6895daeaf
MR Talha Zahid <125808994+MRTalhaZahid@users.noreply.github.com>
2023-04-05 22:50:58 +0500

    Add files via upload

---
+++ talha.js
@@ -0,0 +1,113 @@
+
+//1:write a program maxmium between two number 
+var number1 = prompt (  "Enter the first number " )
+var number2 = prompt ("Enter the second number")
+
+if (number1 > number2 ) {
+    console.log ("first number is greater then second" , number1)
+} else {
+    console.log ("second number is greater then first" , number2)
+}
+
+//2:write a program maxium between 3 number 
+
+// var number1 = prompt("Enter the first number ")
+// var number2 = prompt(" Enter the second number ")
+// var  number3 = prompt("Enter the third number ")
+
+// var max = Math.max (number1,number2,number3)
+
+// console.log ("The maximum number is "+max )
+
+// 3: write the program check weather number is negitive postive or zero
+
+// var check = prompt("Enter the number " )
+
+// if (check>0){
+//     console.log ("the number is positive ",check)
+// }
+// else if(check<0){
+//     console.log("the number is negitive ", check)
+// }
+
+// else {console.log ("The number is zero",check)
+// }
+
+
+// 4: write the program to check whether divisible by 5 and 11 0r not  
+
+
+// var num = prompt("Enter the number ")
+
+//   if (num % 5 == 0 && num % 11 == 0) {
+//     console.log(+num ,"is divided by both 5 and 11")
+//   } else {
+//     console.log(+num ,"is not divided by both 5 and 11")
+//   }
+
+// 5 : write the js program to find number is even or odd
+
+// var num = prompt("Enter the number ")
+// if (num % 2==0) {
+//     console.log (+num, "this is even ")
+    
+// } else {
+//     console.log (+num, "this is Odd ")
+// }
+
+// // 6: write the js program to check whether a year is leap or not
+
+// var year= prompt("Enter the year ")
+
+// if (year % 4 && year %100 || year % 400) {
+    
+//     console.log (+year ,"is leap yaer ")
+// } else {
+//     console.log (+year ,"is not  leap yaer ")
+// }
+
+
+// 7: write the js program to check whether a character is alphabet or not 
+
+// var alpha= prompt("Enter the character ")
+
+// if ((alpha >= 'a' && alpha <= 'z') || (alpha >= 'A' && alpha <= 'Z')) {
+// console.log(+alpha,"this is Alphabet ")    
+// } else {
+//     console.log(+alpha,"this is not Alphabet ")
+// }
+
+// 8: write the js program to input any alphabet and check whether it is vowel or consonant 
+
+
+// var char = prompt("Enter the number ")
+
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+//     console.log(+char,"this is vowel Alphabet ")
+// } else {
+//     console.log(+char,"this is not vowel Alphabet ")
+// }
+
+// 9: write the js program to input any charater and check whether it is alphabet digit or special character 
+
+// var char= prompt("Enter the character ")
+
+// if((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')){
+//     console.log(+char,"this is Alphabet ")
+// }
+// else if ( char >= '0' && char <= '9' ){
+//     console.log (+char,"this is digit")
+// }
+// else {
+//     console.log (+char,"this is special character")
+// }
+
+// 10:Write a js program to check whether a character is uppercase or lowercase alphabet
+
+// var char = prompt("Enter value ")
+// if( (char >= 'A' && char <= 'Z')){
+//         console.log(+char,"this is upparcase Alphabet ")
+//     }
+//     else{
+//         console.log(+char,"this is lowercase Alphabet ")
+//     }
